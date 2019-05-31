6f67a1f1b646864093130f9148bca644d5f61955
Eric Aniag <ericaniag@gmail.com>
2019-05-31 01:22:44 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('form').on('keydown',function() {
+  $('form').on('keydown',function(key) {
     if (key.which === 073) {
       alert('G was pressed')
     }
